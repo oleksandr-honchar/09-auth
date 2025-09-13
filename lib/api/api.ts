5aa6da32bd6284df40c7f4e013701823e63313de
Oleksandr Honchar <figules@gmail.com>
2025-09-13 13:21:03 +0200

    fix path error

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,11 +1,13 @@
-// lib/api/api.ts
 import axios, { AxiosError } from "axios";
 
-// Client-side & SSR API instance with cookies support
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
 
-// Shared error type for catching API errors
+export const nextServer = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL + "/api",
+  withCredentials: true,
+});
+
 export type ApiError = AxiosError<{ error: string }>;
