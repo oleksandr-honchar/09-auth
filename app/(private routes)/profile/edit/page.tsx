bd6345224a5e61e06d9561cc226caf7f6de48479
Oleksandr Honchar <figules@gmail.com>
2025-09-13 13:25:34 +0200

    fix file name error

--- app/(private routes)/profile/edit/page.tsx
+++ app/(private routes)/profile/edit/page.tsx
@@ -3,8 +3,7 @@
 import { useState, useEffect, FormEvent } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-
-import css from "./ProfileEditPage.module.css";
+import css from "./EditProfilePage.module.css";
 import { useAuthStore } from "@/lib/store/authStore";
 import { updateUserProfile } from "@/lib/api/clientApi";
 
